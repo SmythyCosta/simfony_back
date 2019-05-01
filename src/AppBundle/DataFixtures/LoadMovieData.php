56dfcafe3554702c96bd5a733e3860aa9add694e
Smythy Costa <smythy.costa@hotmail.com>
2019-05-01 00:48:40 -0300

    Seeds Movie

--- src/AppBundle/DataFixtures/LoadMovieData.php
+++ src/AppBundle/DataFixtures/LoadMovieData.php
@@ -10,11 +10,24 @@ class LoadMovieData extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+
+        $movie0 = new Movie();
+        $movie0->setTitle('Loucuras na idade Média');
+        $movie0->setDescription('filme de comedia, top 5 estrelas');
+
         $movie1 = new Movie();
-        $movie1->setTitle('Loucuras na Idade Média');
-        $movie1->setDescription('Um bon filme de comédia, 5 estrelas...');
+        $movie1->setTitle('O Resgate do Soldado Ryan');
+        $movie1->setDescription('filme de guerra épico norte-americano de 1998, ambientado durante a Batalha da Normandia na Segunda Guerra Mundial.');
         
+        $movie2 = new Movie();
+        $movie2->setTitle('Matrix');
+        $movie2->setDescription('O filme descreve um futuro distópico no qual a realidade, como percebida pela maioria dos humanos, é, na verdade, uma realidade simulada chamada "Matrix", criada por máquinas sencientes para subjugar a população humana, enquanto o calor e a atividade elétrica de seus corpos são usados ​​como fonte de energia.');
+        
+        $manager->persist($movie0);
         $manager->persist($movie1);
+        $manager->persist($movie2);
         $manager->flush();
+
     }
+    
 }
